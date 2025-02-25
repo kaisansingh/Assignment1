832618a7fb00e94dfc07683d2b3f65023a2891a3
kaisansingh <kaisan619@gmail.com>
2025-02-25 13:37:53 +0530

    task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2: Personalized Greeting Program
+
+# Take user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate to form full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print(f"Hello, {full_name}! Welcome!")
